9694ff4592fb537994cbe92de9de051c95d7cdd6
quiky <quikycz@gmail.com>
2015-03-22 16:03:29 +0100

    sql scripts

---
+++ sql/init.sql
@@ -0,0 +1,23 @@
+------------------------------------ CUSTOMER ------------------------------------
+
+CREATE TABLE CUSTOMER (
+  ID          BIGINT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  IDCARD      VARCHAR(50) NOT NULL,
+  "NAME"      VARCHAR(50) NOT NULL,
+  ADDRESS     VARCHAR(50) NOT NULL,
+  TELEPHONE   VARCHAR(50) NOT NULL,
+  EMAIL       VARCHAR(50) NOT NULL
+);
+
+------------------------------------ BOOK ------------------------------------
+
+CREATE TABLE BOOK (
+  ID            INTEGER NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  ISBN          VARCHAR(50) NOT NULL,
+  "NAME"        VARCHAR(50) NOT NULL,
+  AUTHOR        VARCHAR(50) NOT NULL,
+  PUBLISHER     VARCHAR(50) NOT NULL,
+  "YEAR"        INTEGER NOT NULL,
+  "LANGUAGE"      VARCHAR(50) NOT NULL,
+  PAGESNUMBER   INTEGER NOT NULL
+);
