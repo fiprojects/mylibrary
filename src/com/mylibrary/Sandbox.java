f3f8f9535994f2b41c5dee3a2e6d6e1e6c2c9016
lemichael <mail@michael-le.net>
2015-04-27 04:11:07 +0200

    I18N

---
+++ src/com/mylibrary/Sandbox.java
@@ -0,0 +1,28 @@
+package com.mylibrary;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Sandbox {
+    private final static Logger log = LoggerFactory.getLogger(Sandbox.class);
+
+    public static void main(String[] args) {
+        logging();
+        i18n();
+    }
+
+    public static void logging() {
+        log.debug("I ty debuzire!");
+        log.info("Informuji te!");
+        log.warn("Dokonce te varuji!");
+        log.error("Mas tam chybu!");
+    }
+
+    public static void i18n() {
+        ResourceBundle lang = ResourceBundle.getBundle("i18n", new Locale("cs_CZ"));
+        System.out.println(lang.getString("name"));
+    }
+}
